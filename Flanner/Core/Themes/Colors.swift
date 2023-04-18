ea4402cb55875e2b89388505f4091153e793860f
Kevin Bryan <kevinbryanreligion@gmail.com>
2023-04-18 16:06:41 +0700

    add: Color variables

--- Flanner/Core/Themes/Colors.swift
+++ Flanner/Core/Themes/Colors.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import SwiftUI
+
+let cream = Color("#cream")
+let turq = Color("#turq")
+let lYellow = Color("#lightYellow")
+let dYellow = Color("#darkYellow")
+
+let white = Color.white
+let black = Color.black
+
