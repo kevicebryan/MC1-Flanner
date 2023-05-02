7e683ab3c7bee6a8d1bdbf6fdc353df0f18dfc48
Kevin Bryan <kevinbryanreligion@gmail.com>
2023-05-02 18:04:13 +0700

    fix: spacing for PlanBridge

--- Flanner/Features/View/PlanBridgeView.swift
+++ Flanner/Features/View/PlanBridgeView.swift
@@ -8,40 +8,41 @@
 import SwiftUI
 
 struct PlandBridgeView: View {
-    var body: some View {
-        NavigationView {
-            ZStack {
-                Colors.turq
-                    .edgesIgnoringSafeArea(.all)
-                VStack {
-                    Spacer()
-                    
-                    Image("dummyImage")
-                        .resizable()
-                        .frame(width: 279, height: 279)
-                    Text("Let’s Plan Your Activity!")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(Colors.cream)
-                        .font(.system(size: 28))
-                    Text("Fill in the questions below to generate the best activity to your current mood and preferences")
-                        .fontWeight(.regular)
-                        .font(.system(size: 12))
-                        .foregroundColor(Colors.cream)
-                        .multilineTextAlignment(.center)
-                    Spacer()
-                    
-                    NavigationLink(destination: ThisOrThatView()
-                    ) {CustomButton(label: "START")
-                        }
-                    }
-                }
-            }
+  var body: some View {
+    NavigationView {
+      ZStack {
+        Colors.turq
+          .edgesIgnoringSafeArea(.all)
+        VStack {
+          Spacer()
+
+          Image("dummyImage")
+            .resizable()
+            .frame(width: 279, height: 279)
+          Text("Let’s Plan Your Activity!")
+            .font(.title)
+            .fontWeight(.bold)
+            .foregroundColor(Colors.cream)
+            .font(.system(size: 28)).padding(.top, 20)
+          Text("Fill in the questions below to generate the best activity to your current mood and preferences")
+            .fontWeight(.regular)
+            .font(.system(size: 12))
+            .foregroundColor(Colors.creamTint)
+            .multilineTextAlignment(.center)
+            .frame(width: 300)
+
+          NavigationLink(destination: ThisOrThatView()
+          ) { CustomButton(label: "Start", width: 300)
+          }
+          Spacer()
         }
+      }
     }
+  }
+}
 
 struct PlandBridgeView_Previews: PreviewProvider {
-    static var previews: some View {
-        PlandBridgeView()
-    }
+  static var previews: some View {
+    PlandBridgeView()
+  }
 }
