a8796e6048a66b4ace9a0fd739aa2bd383f76775
Kevin Bryan <kevinbryanreligion@gmail.com>
2023-05-02 17:02:31 +0700

    add new Coloring System

--- Flanner/Features/View/Components/CustomButton.swift
+++ Flanner/Features/View/Components/CustomButton.swift
@@ -8,46 +8,28 @@
 import SwiftUI
 
 struct CustomButton: View {
-    let label: String
-    let width: Int
-    let isDisabled: Bool
-    
-    
-    init(label: String, width: Int = 200, isDisabled: Bool = false) {
-        self.label = label
-        self.width = width
-        self.isDisabled = isDisabled
-    }
-    
-    var body: some View {
-        Text("\(label)")
-            .foregroundColor(.white)
-            .frame(width: CGFloat(width), height: 40, alignment: .center)
-            .background((isDisabled) ? Colors.disabledDYellow : Colors.dYellow)
-            .cornerRadius(20, antialiased: true)
-            .padding()
-    }
   let label: String
   let width: Int
+  let isDisabled: Bool
 
-  init(label: String, width: Int = 200) {
+  init(label: String, width: Int = 200, isDisabled: Bool = false) {
     self.label = label
     self.width = width
+    self.isDisabled = isDisabled
   }
 
   var body: some View {
     Text("\(label)")
-      .fontWeight(.semibold).font(.footnote)
-      .foregroundColor(Colors.creamTint)
+      .foregroundColor(.white)
       .frame(width: CGFloat(width), height: 48, alignment: .center)
-      .background(Colors.yellow)
+      .background(isDisabled ? Colors.yellowTint : Colors.yellowShade)
       .cornerRadius(20, antialiased: true)
       .padding()
   }
 }
 
 struct CustomButton_Previews: PreviewProvider {
-    static var previews: some View {
-        CustomButton(label: "This is the label", isDisabled: true)
-    }
+  static var previews: some View {
+    CustomButton(label: "This is the label", isDisabled: true)
+  }
 }
--- Flanner/Features/View/ThisOrThatView.swift
+++ Flanner/Features/View/ThisOrThatView.swift
@@ -8,148 +8,146 @@
 import SwiftUI
 
 struct ThisOrThatView: View {
-    let buttonWidth = Size.screenWidth/1.3
-    let buttonHeight = Size.screenHeight/5
-    @State var index = 0
-    @State var isSelected1 = false
-    @State var isSelected2 = false
+  let buttonWidth = Size.screenWidth/1.3
+  let buttonHeight = Size.screenHeight/5
+  @State var index = 0
+  @State var isSelected1 = false
+  @State var isSelected2 = false
     
-    var body: some View {
-        NavigationView {
-            VStack (alignment: .center) {
-                Text("GOAL & FOCUS 🎯")
-                    .foregroundColor(Colors.turq)
-                    .font(.system(size: 12, weight: .bold))
-                ProgressBar(index: $index)
+  var body: some View {
+    NavigationView {
+      VStack(alignment: .center) {
+        Text("GOAL & FOCUS 🎯")
+          .foregroundColor(Colors.turq)
+          .font(.system(size: 12, weight: .bold))
+        ProgressBar(index: $index)
 //                    .frame(alignment: .top)
-                Spacer()
-                Text("\(Prompt.thisOrThatPrompts[index])")
-                    .font(.system(size: 24, weight: .bold))
-                    .frame(width: buttonWidth)
-                    .multilineTextAlignment(.center)
+        Spacer()
+        Text("\(Prompt.thisOrThatPrompts[index])")
+          .font(.system(size: 24, weight: .bold))
+          .frame(width: buttonWidth)
+          .multilineTextAlignment(.center)
                 
-                ZStack {
-                    if !isSelected1 {
-                        Rectangle()
-                            .foregroundColor(Colors.lBlue)
-                            .cornerRadius(20, antialiased: true)
+        ZStack {
+          if !isSelected1 {
+            Rectangle()
+              .foregroundColor(Colors.lBlue)
+              .cornerRadius(20, antialiased: true)
                         
-                        HStack {
-                            Image(systemName: "\(Prompt.thisOrThatSymbols[index][0])")
-                                .font(.system(size: 60))
-                                .padding()
-                                .foregroundColor(Colors.turq)
-                            Text("\(Prompt.thisOrThatChoices[index][0])")
-                                .foregroundColor(Colors.turq)
-                                .padding()
-//                                .frame(width: buttonWidth/2)
-                                .font(.system(size: 16, weight: .bold))
-                        }
-                    }
-                    else {
-                        Rectangle()
-                            .foregroundColor(Colors.turq)
-                            .cornerRadius(20, antialiased: true)
-                        
-                        HStack {
-                            Image(systemName: "\(Prompt.thisOrThatSymbols[index][0])")
-                                .font(.system(size: 60))
-                                .padding()
-                                .foregroundColor(Colors.cream)
-                            Text("\(Prompt.thisOrThatChoices[index][0])")
-                                .foregroundColor(Colors.cream)
-                                .padding()
-//                                .frame(width: buttonWidth/2)
-                                .font(.system(size: 16, weight: .bold))
-                        }
-                    }
-                }
-                .onTapGesture {
-                    if isSelected2 {
-                        isSelected2 = false
-                        isSelected1 = true
-                    }
-                    else {
-                        isSelected1 = true
-                    }
-                }
-                .frame(width: buttonWidth, height: buttonHeight, alignment: .center)
+            HStack {
+              Image(systemName: "\(Prompt.thisOrThatSymbols[index][0])")
+                .font(.system(size: 60))
                 .padding()
-                
-                ZStack {
-                    if !isSelected2 {
-                        Rectangle()
-                            .foregroundColor(Colors.lBlue)
-                            .cornerRadius(20, antialiased: true)
-                        
-                        HStack {
-                            Image(systemName: "\(Prompt.thisOrThatSymbols[index][1])")
-                                .foregroundColor(Colors.turq)
-                                .font(.system(size: 60))
-                                .padding()
-                            Text("\(Prompt.thisOrThatChoices[index][1])")
-                                .foregroundColor(Colors.turq)
-                                .padding()
+                .foregroundColor(Colors.turq)
+              Text("\(Prompt.thisOrThatChoices[index][0])")
+                .foregroundColor(Colors.turq)
+                .padding()
 //                                .frame(width: buttonWidth/2)
-                                .font(.system(size: 16, weight: .bold))
-                        }
-                    }
-                    else {
-                        Rectangle()
-                            .foregroundColor(Colors.turq)
-                            .cornerRadius(20, antialiased: true)
-                        
-                        HStack {
-                            Image(systemName: "\(Prompt.thisOrThatSymbols[index][1])")
-                                .font(.system(size: 60))
-                                .padding()
-                                .foregroundColor(Colors.cream)
-                            Text("\(Prompt.thisOrThatChoices[index][1])")
-                                .foregroundColor(Colors.cream)
-                                .padding()
-//                                .frame(width: buttonWidth/2)
-                                .font(.system(size: 16, weight: .bold))
-                        }
-                    }
-                }
-                .frame(width: buttonWidth, height: buttonHeight, alignment: .center)
-                .onTapGesture {
-                    if isSelected1 {
-                        isSelected1 = false
-                        isSelected2 = true
-                    }
-                    else {
-                        isSelected2 = true
-                    }
-                }
-                Spacer()
-                if(index != 4){
-                    Button{
-                        index += 1
-                        isSelected1 = false
-                        isSelected2 = false
-                    } label: {
-                        CustomButton(label: "Next", isDisabled: (!isSelected1 && !isSelected2))
-                       
-                    }
-                    .disabled(!isSelected1 && !isSelected2)
-                        .padding()
-                }
-                else{
-                    NavigationLink(destination: HomeView().navigationBarBackButtonHidden(true)){
-                        CustomButton(label: "Continue", isDisabled: (!isSelected1 && !isSelected2))
-                            .disabled(!isSelected1 && !isSelected2)
-                                .padding()
-                    }
-                }
-                
+                .font(.system(size: 16, weight: .bold))
             }
+          }
+          else {
+            Rectangle()
+              .foregroundColor(Colors.turq)
+              .cornerRadius(20, antialiased: true)
+                        
+            HStack {
+              Image(systemName: "\(Prompt.thisOrThatSymbols[index][0])")
+                .font(.system(size: 60))
+                .padding()
+                .foregroundColor(Colors.cream)
+              Text("\(Prompt.thisOrThatChoices[index][0])")
+                .foregroundColor(Colors.cream)
+                .padding()
+//                                .frame(width: buttonWidth/2)
+                .font(.system(size: 16, weight: .bold))
+            }
+          }
         }
+        .onTapGesture {
+          if isSelected2 {
+            isSelected2 = false
+            isSelected1 = true
+          }
+          else {
+            isSelected1 = true
+          }
+        }
+        .frame(width: buttonWidth, height: buttonHeight, alignment: .center)
+        .padding()
+                
+        ZStack {
+          if !isSelected2 {
+            Rectangle()
+              .foregroundColor(Colors.lBlue)
+              .cornerRadius(20, antialiased: true)
+                        
+            HStack {
+              Image(systemName: "\(Prompt.thisOrThatSymbols[index][1])")
+                .foregroundColor(Colors.turq)
+                .font(.system(size: 60))
+                .padding()
+              Text("\(Prompt.thisOrThatChoices[index][1])")
+                .foregroundColor(Colors.turq)
+                .padding()
+//                                .frame(width: buttonWidth/2)
+                .font(.system(size: 16, weight: .bold))
+            }
+          }
+          else {
+            Rectangle()
+              .foregroundColor(Colors.turq)
+              .cornerRadius(20, antialiased: true)
+                        
+            HStack {
+              Image(systemName: "\(Prompt.thisOrThatSymbols[index][1])")
+                .font(.system(size: 60))
+                .padding()
+                .foregroundColor(Colors.cream)
+              Text("\(Prompt.thisOrThatChoices[index][1])")
+                .foregroundColor(Colors.cream)
+                .padding()
+//                                .frame(width: buttonWidth/2)
+                .font(.system(size: 16, weight: .bold))
+            }
+          }
+        }
+        .frame(width: buttonWidth, height: buttonHeight, alignment: .center)
+        .onTapGesture {
+          if isSelected1 {
+            isSelected1 = false
+            isSelected2 = true
+          }
+          else {
+            isSelected2 = true
+          }
+        }
+        Spacer()
+        if index != 4 {
+          Button {
+            index += 1
+            isSelected1 = false
+            isSelected2 = false
+          } label: {
+            CustomButton(label: "Next", isDisabled: !isSelected1 && !isSelected2)
+          }
+          .disabled(!isSelected1 && !isSelected2)
+          .padding()
+        }
+        else {
+          NavigationLink(destination: HomeView(um: UserManager()).navigationBarBackButtonHidden(true)) {
+            CustomButton(label: "Continue", isDisabled: !isSelected1 && !isSelected2)
+              .disabled(!isSelected1 && !isSelected2)
+              .padding()
+          }
+        }
+      }
     }
+  }
 }
 
 struct ThisOrThatView_Previews: PreviewProvider {
-    static var previews: some View {
-        ThisOrThatView(index: 4)
-    }
+  static var previews: some View {
+    ThisOrThatView(index: 4)
+  }
 }
