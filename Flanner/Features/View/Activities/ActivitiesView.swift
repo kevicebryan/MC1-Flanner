f4fc1d5f07b16135e0b40d6e8fa52626d8d8d466
Kevin Bryan <kevinbryanreligion@gmail.com>
2023-05-07 00:17:33 +0700

    fix: auto refresh for wishlistview to activityView

--- Flanner/Features/View/Activities/ActivitiesView.swift
+++ Flanner/Features/View/Activities/ActivitiesView.swift
@@ -8,48 +8,47 @@
 import SwiftUI
 
 struct ActivitiesView: View {
-    @State private var index = 0
-    @StateObject var tlvm = TaskListViewModel()
-    
-    var body: some View {
-        NavigationView {
-            VStack {
-                Text("Our Activities")
-                    .fontWeight(.bold)
-                    .foregroundColor(Colors.turq)
-                    .font(.system(size: 22))
-                    .padding(.top, 15)
-                
-                Picker("Select", selection: $index) {
-                    Text("Wishlist").tag(0)
-                    Text("Done").tag(1)
-                }
-                .pickerStyle(.segmented)
-                .padding()
-                
-                
-                if index == 0 {
-                    ScrollView {
-                        ForEach(tlvm.plannedTasks, id: \.self.id) { task in
-                            WishlistView(record: task)
-                        }
-                    }.ignoresSafeArea()
-                    
-                } else {
-                    ScrollView {
-                        ForEach(tlvm.doneTasks, id: \.self.id) { task in
-                            DoneActivitiesView(record: task, isReviewed: .constant(false))
-                        }
-                    }.ignoresSafeArea()
-                }
-                Spacer()
-            }
+  @State private var index = 0
+  @StateObject var tlvm = TaskListViewModel()
+
+  var body: some View {
+    NavigationView {
+      VStack {
+        Text("Our Activities")
+          .fontWeight(.bold)
+          .foregroundColor(Colors.turq)
+          .font(.system(size: 22))
+          .padding(.top, 15)
+
+        Picker("Select", selection: $index) {
+          Text("Wishlist").tag(0)
+          Text("Done").tag(1)
         }
+        .pickerStyle(.segmented)
+        .padding()
+
+        if index == 0 {
+          ScrollView {
+            ForEach(tlvm.plannedTasks, id: \.self.id) { task in
+              WishlistView(tlvm: tlvm, record: task)
+            }
+          }.ignoresSafeArea()
+
+        } else {
+          ScrollView {
+            ForEach(tlvm.doneTasks, id: \.self.id) { task in
+              DoneActivitiesView(tlvm: tlvm, record: task, isReviewed: .constant(false))
+            }
+          }.ignoresSafeArea()
+        }
+        Spacer()
+      }
     }
+  }
 }
 
 struct ActivitiesView_Previews: PreviewProvider {
-    static var previews: some View {
-        ActivitiesView()
-    }
+  static var previews: some View {
+    ActivitiesView()
+  }
 }
--- Flanner/Features/View/Activities/WishlistView.swift
+++ Flanner/Features/View/Activities/WishlistView.swift
@@ -8,51 +8,52 @@
 import SwiftUI
 
 struct WishlistView: View {
-    @State public var record: TaskViewModel
-    @State public var isChecked = false
-    
-    var body: some View {
-        HStack(spacing: 16) {
-            Image(record.image)
-                .resizable()
-                .frame(width: 90, height: 90)
-                .cornerRadius(16)
-                .scaledToFill()
-                .padding(.vertical, 8)
-                .padding(.leading, 16)
-            VStack(alignment: .leading) {
-                Text(record.name).font(
-                    record.name.count >= 12 ? .title2 : .title
-                ).fontWeight(.bold)
-                Text(record.name)
-                    .opacity(0.6)
-                    .fontWeight(.light)
-                    .font(.caption2)
-                    .lineLimit(3, reservesSpace: true)
-            }.padding(.all, 8)
-                .padding(.leading, -16)
-                .frame(width: 180)
-            
-            Image(systemName: isChecked ? "checkmark.square" : "square")
-                .resizable()
-                .frame(width: 20, height: 20)
-                .foregroundColor(isChecked ? Colors.turq : Color.secondary)
-                .onTapGesture {
-                    self.isChecked.toggle()
-                    record.task.done = true
-                    record.task.planned = false
-                    TaskListViewModel().getAllTask()
-                }
-            Spacer()
-        }.background(.white)
-            .cornerRadius(20)
-            .frame(width: 315, height: 116)
-            .shadow(color: Color.black.opacity(0.05), radius: 20, x: 0, y: 20)
-    }
+  @ObservedObject var tlvm: TaskListViewModel
+  @State public var record: TaskViewModel
+  @State public var isChecked = false
+
+  var body: some View {
+    HStack(spacing: 16) {
+      Image(record.image)
+        .resizable()
+        .frame(width: 90, height: 90)
+        .cornerRadius(16)
+        .scaledToFill()
+        .padding(.vertical, 8)
+        .padding(.leading, 16)
+      VStack(alignment: .leading) {
+        Text(record.name).font(
+          record.name.count >= 12 ? .title2 : .title
+        ).fontWeight(.bold)
+        Text(record.name)
+          .opacity(0.6)
+          .fontWeight(.light)
+          .font(.caption2)
+          .lineLimit(3, reservesSpace: true)
+      }.padding(.all, 8)
+        .padding(.leading, -16)
+        .frame(width: 180)
+
+      Image(systemName: isChecked ? "checkmark.square" : "square")
+        .resizable()
+        .frame(width: 20, height: 20)
+        .foregroundColor(isChecked ? Colors.turq : Color.secondary)
+        .onTapGesture {
+          self.isChecked.toggle()
+          record.task.done = true
+          record.task.planned = false
+          tlvm.getAllTask()
+        }
+      Spacer()
+    }.background(.white)
+      .cornerRadius(20)
+      .frame(width: 315, height: 116)
+      .shadow(color: Color.black.opacity(0.05), radius: 20, x: 0, y: 20)
+  }
 }
 
 struct WishlistView_Previews: PreviewProvider {
-    static var previews: some View {
-        WishlistView(record: TaskListViewModel().tasks[0])
-    }
+  static var previews: some View {
+    WishlistView(tlvm: TaskListViewModel(), record: TaskListViewModel().tasks[0])
+  }
 }
